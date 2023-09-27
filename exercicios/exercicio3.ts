8b7861cbe31421bb45a3276f1e54162b6b83d512
Augusto <ampiatto@hotmail.com>
2023-09-27 05:11:58 -0300

    exercicio 3

---
+++ exercicios/exercicio3.ts
@@ -0,0 +1,13 @@
+function toNumber(value: number | string) {
+  if (typeof value === "number") {
+    return value;
+  } else if (typeof value === "string") {
+    return Number(value);
+  } else {
+    throw "value deve ser um número ou uma string";
+  }
+}
+
+console.log(toNumber(1));
+console.log(toNumber("1"));
+console.log(toNumber({}));
