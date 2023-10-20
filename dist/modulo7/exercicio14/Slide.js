b83b7f0acb2e4fc2e63a2009ef7d0c85a6ff766b
Augusto <ampiatto@hotmail.com>
2023-10-20 02:32:51 -0300

    estrutura a parte de proxximo e anterior no carroussel

--- dist/modulo7/exercicio14/Slide.js
+++ dist/modulo7/exercicio14/Slide.js
@@ -1,13 +1,81 @@
+import Timeout from "./Timeout.js";
 export default class Slide {
     container;
-    elements;
+    slides;
     controls;
     time;
-    constructor(container, elements, controls, time = 5000) {
+    index;
+    slide;
+    timeout;
+    pausedTimeout;
+    paused;
+    constructor(container, slides, controls, time = 5000) {
         this.container = container;
-        this.elements = elements;
+        this.slides = slides;
         this.controls = controls;
         this.time = time;
+        this.timeout = null;
+        this.pausedTimeout = null;
+        this.index = 0;
+        this.slide = this.slides[this.index];
+        this.paused = false;
+        this.init();
+    }
+    hide(el) {
+        el.classList.remove("active");
+    }
+    show(index) {
+        this.index = index;
+        this.slide = this.slides[this.index];
+        this.slides.forEach((slide) => this.hide(slide));
+        this.slide.classList.add("active");
+        this.auto(this.time);
+    }
+    auto(time) {
+        this.timeout?.clear();
+        this.timeout = new Timeout(() => this.next(), time);
+    }
+    pause() {
+        this.pausedTimeout = new Timeout(() => {
+            this.paused = true;
+        }, 300);
+        this.timeout?.pause();
+        this.paused = true;
+    }
+    continue() {
+        this.pausedTimeout?.clear();
+        if (this.paused) {
+            this.paused = false;
+            this.timeout?.continue();
+        }
+    }
+    previous() {
+        if (this.paused)
+            return;
+        const previous = this.index > 0 ? this.index - 1 : this.slides.length - 1;
+        this.show(previous);
+    }
+    next() {
+        if (this.paused)
+            return;
+        const next = this.index + 1 < this.slides.length ? this.index + 1 : 0;
+        this.show(next);
+    }
+    addControls() {
+        const prevButton = document.createElement("button");
+        const nextButton = document.createElement("button");
+        prevButton.innerText = "Slide Anterior";
+        nextButton.innerText = "Próximo Slide";
+        this.controls.appendChild(prevButton);
+        this.controls.appendChild(nextButton);
+        this.controls.addEventListener("pointerdown", () => this.pause());
+        this.controls.addEventListener("pointerup", () => this.continue());
+        prevButton.addEventListener("pointerup", () => this.previous());
+        nextButton.addEventListener("pointerup", () => this.next());
+    }
+    init() {
+        this.addControls();
+        this.show(this.index);
     }
 }
 //# sourceMappingURL=Slide.js.map
--- src/modulo7/exercicio14/Slide.ts
+++ src/modulo7/exercicio14/Slide.ts
@@ -1,17 +1,98 @@
+import Timeout from "./Timeout.js";
+
 export default class Slide {
   container;
-  elements;
+  slides;
   controls;
   time;
+  index: number;
+  slide: Element;
+  timeout: Timeout | null;
+  pausedTimeout: Timeout | null;
+  paused: boolean;
+
   constructor(
     container: Element,
-    elements: Element[],
+    slides: Element[],
     controls: Element,
     time: number = 5000
   ) {
     this.container = container;
-    this.elements = elements;
+    this.slides = slides;
     this.controls = controls;
     this.time = time;
+
+    this.timeout = null;
+    this.pausedTimeout = null;
+    this.index = 0;
+    this.slide = this.slides[this.index];
+
+    this.paused = false;
+    this.init();
+  }
+
+  hide(el: Element) {
+    el.classList.remove("active");
+  }
+
+  show(index: number) {
+    this.index = index;
+    this.slide = this.slides[this.index];
+    this.slides.forEach((slide) => this.hide(slide));
+    this.slide.classList.add("active");
+    this.auto(this.time);
+  }
+
+  auto(time: number) {
+    this.timeout?.clear();
+    this.timeout = new Timeout(() => this.next(), time);
+  }
+
+  pause() {
+    this.pausedTimeout = new Timeout(() => {
+      this.paused = true;
+    }, 300);
+    this.timeout?.pause();
+    this.paused = true;
+  }
+
+  continue() {
+    this.pausedTimeout?.clear();
+    if (this.paused) {
+      this.paused = false;
+      this.timeout?.continue();
+    }
+  }
+
+  previous() {
+    if (this.paused) return;
+    const previous = this.index > 0 ? this.index - 1 : this.slides.length - 1;
+    this.show(previous);
+  }
+
+  next() {
+    if (this.paused) return;
+    const next = this.index + 1 < this.slides.length ? this.index + 1 : 0;
+    this.show(next);
+  }
+
+  private addControls() {
+    const prevButton = document.createElement("button");
+    const nextButton = document.createElement("button");
+    prevButton.innerText = "Slide Anterior";
+    nextButton.innerText = "Próximo Slide";
+    this.controls.appendChild(prevButton);
+    this.controls.appendChild(nextButton);
+
+    this.controls.addEventListener("pointerdown", () => this.pause());
+    this.controls.addEventListener("pointerup", () => this.continue());
+
+    prevButton.addEventListener("pointerup", () => this.previous());
+    nextButton.addEventListener("pointerup", () => this.next());
+  }
+
+  private init() {
+    this.addControls();
+    this.show(this.index);
   }
 }
